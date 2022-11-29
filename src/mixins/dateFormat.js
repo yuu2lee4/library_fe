e1f7ff4a5b8e870a476b2140e195c994b922e8de
yuri <yuu2lee4@qq.com>
2022-11-29 16:18:00 +0800

    更新依赖

--- src/mixins/dateFormat.js
+++ src/mixins/dateFormat.js
@@ -2,7 +2,7 @@ import format from 'date-fns/format';
 
 export default {
     methods: {
-        dateFormat(time, fmt = 'YYYY-MM-DD HH:mm:ss') {
+        dateFormat(time, fmt = 'yyyy-MM-dd HH:mm:ss') {
             return format(new Date(time), fmt);
         },
     },
